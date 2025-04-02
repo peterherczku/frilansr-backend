c701c9ca9f10fa2b39d4fc63c81c20e405dcfd06
Péter Herczku <herczku@kth.se>
2025-04-02 19:48:49 +0200

    feat: render yaml

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import { createServer } from "http";
 import app from "./app.js"; // imported from app.ts
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const server = createServer(app);
 
 server.listen(PORT, () => {
