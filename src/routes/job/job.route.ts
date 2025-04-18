4ffc571a7dd9493111244318f625fda2ad209277
Péter Herczku <herczku@kth.se>
2025-04-18 17:31:58 +0200

    ft: changes

--- src/routes/job/job.route.ts
+++ src/routes/job/job.route.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import jobIdRoutes from "./job.id.routes.js";
 import { requireJobId } from "../../middlewares/jobIdMiddleware.js";
+import { requireAuth } from "../../middlewares/authMiddleware.js";
 
 const router = Router();
 
-router.get("/");
+router.get("/", requireAuth);
 router.use("/:id", requireJobId, jobIdRoutes);
 
 export default router;
