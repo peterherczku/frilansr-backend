00826e3b9feb66d6fdc70a7bc684982df4b67fbb
Péter Herczku <herczku@kth.se>
2025-04-20 01:39:13 +0200

    ft: bounded seen route

--- src/routes/message/message.route.ts
+++ src/routes/message/message.route.ts
@@ -6,11 +6,13 @@ import {
 	getRecentConversations,
 	sendMessage,
 } from "../../controllers/message/message.controller.js";
+import { sendSeen } from "../../services/message.service.js";
 
 const router = Router();
 
 router.get("/conversation", requireAuth, getConversation);
 router.get("/recent-conversations", requireAuth, getRecentConversations);
+router.post("/seen", requireAuth, sendSeen);
 router.post("/", requireAuth, sendMessage);
 router.get("/", requireAuth, getMessages);
 
